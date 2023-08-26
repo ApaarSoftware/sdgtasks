590bc28feab066f3f5c0c48276f396f858a06999
ApaarSoftware <131344722+ApaarSoftware@users.noreply.github.com>
2023-08-26 15:43:17 +0530

    adding files

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
