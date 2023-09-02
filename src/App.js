3af4770fe1bda38a053beb2c8efd2c37454ff8a4
Yajuvender <Yajuvender99@gmail.com>
2023-09-02 13:30:27 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,28 +4,47 @@ import Home from './components/Home';
 import About from './components/About';
 import './App.css';
 import UpdateGoogleSheet from './components/UpdateGoogleSheet';
+import UpdateCompletedDateTime from './components/UpdateCompletedDateTime';
 
 function App() {
+
+  //   const location = useLocation();
+  const queryParams = new URLSearchParams(document.location.search);
+  const TaskId = queryParams.get('TaskId');
+//   const TaskDesc = queryParams.get('TaskDesc');
+  const cellNo = queryParams.get('CellNo');
+  const completedCellNo = queryParams.get('completedCellNo');
+  
+  console.log('queryParams TaskId : ' + TaskId);
+  console.log('queryParams cellNo : ' + cellNo);
+  console.log('queryParams completedCellNo : ' + completedCellNo);
+
+
   return (
     <Router>
+      
       <div className="App">
-        {/* <nav>
+        <nav>
           <ul>
-            <li>
+            {/* <li>
               <Link to="/">Home</Link>
+            </li> */}
+            {/* <li>
+              <Link to="about">About</Link>
+            </li> */}
+            <li>
+              <Link to="setdate">Set Deadline</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/setdate">Set Deadline</Link>
+              <Link to="markcomplete">Mark as completed</Link>
             </li>
           </ul>
-        </nav> */}
+        </nav>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/setdate" element={<UpdateGoogleSheet />} />
+          {/* <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} /> */}
+          <Route path="setdate" element={<UpdateGoogleSheet />} />
+          <Route path="markcomplete" element={<UpdateCompletedDateTime />} />
         </Routes>
       </div>
     </Router>
